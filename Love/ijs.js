19cda5b437b2780bd00c776505ab8adf8aa44a6d
Akshayak603 <44517979+Akshayak603@users.noreply.github.com>
2020-12-07 11:39:11 +0530

    trans

--- Love/ijs.js
+++ Love/ijs.js
@@ -1,4 +1,17 @@
+$(document).on("scroll", function () {
+var pageTop = $(document).scrollTop()
+var pageBottom = pageTop + $(window).height()
+var tags = $("div")
 
+for (var i = 0; i < tags.length; i++) {
+var tag = tags[i]
+
+if ($(tag).position().top < pageBottom) {
+$(tag).addClass("visible")
+} else {  $(tag).removeClass("visible")
+}
+}
+})
 
 var btn = document.querySelectorAll(".btn-outline-danger");
 for(var i=0;i<btn.length;i++){
--- in.js
+++ in.js
@@ -12,11 +12,3 @@ $(tag).addClass("visible")
 }
 }
 })
-
-var btn = document.querySelectorAll(".btn-outline-danger");
-for(var i=0;i<btn.length;i++){
-btn[i].addEventListener("click", function(){
-	this.classList.toggle("active");
-});
-};
-
